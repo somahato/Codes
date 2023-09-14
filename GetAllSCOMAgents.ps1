4763e4f2089f780cd2e17dfc7d664ce09f042b66
Sourav Mahato <95037368+somahato@users.noreply.github.com>
2023-09-14 10:36:29 +0530

    Update GetAllSCOMAgents.ps1
    Added to get Unix server details as well.

--- GetAllSCOMAgents.ps1
+++ GetAllSCOMAgents.ps1
@@ -1,14 +1,38 @@
 $Cur = Get-Location
-Remove-Item -path "$Cur\Agentlist.txt"
-Import-Module OperationsManager
-$Agentlist = Get-scomagent
-foreach ($Agent in $Agentlist)
+if (Test-Path "$Cur\Agentlist.txt")
+        {
+            Remove-Item -Path "$Cur\Agentlist.txt" -Force
+        }
 
+if (Test-Path "$Cur\UnixAgentlist.txt")
+        {
+            Remove-Item -Path "$Cur\UnixAgentlist.txt" -Force
+        }
+
+Import-Module OperationsManager
+
+$Agentlist = Get-SCOMAgent
+
+foreach ($Agent in $Agentlist)
 {
+
 $DisplayName=$Agent.Displayname
 $IPAddress=$Agent.IPAddress
 $HealthState=$Agent.HealthState
 $Domain = $Agent.Domain
 
-"ServerName =""$DisplayName"",IPAddress=""$IPAddress"",Domain=""$Domain"",HealthState=""$HealthState""" >> "$Cur\Agentlist.txt"
+"DisplayName=""$DisplayName"",IPAddress=""$IPAddress"",HealthState=""$HealthState""" >> $Cur\Agentlist.txt
+}
+
+$SCXAgentlist = Get-SCOMClass -Displayname “UNIX/Linux Computer” | Get-SCOMClassInstance
+
+foreach ($Agent in $SCXAgentlist)
+{
+
+$DisplayName=$Agent.Displayname
+$IPAddress=$Agent.'[Microsoft.Unix.Computer].IPAddress'
+$HealthState=$Agent.HealthState
+$Domain = $Agent.Domain
+
+"DisplayName =""$DisplayName"",IPAddress=""$IPAddress"",HealthState=""$HealthState""" >> $Cur\Agentlist.txt
 }
