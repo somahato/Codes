329cf9710ca940746a371f7c9a6ca30f227c8792
Sourav Mahato <95037368+somahato@users.noreply.github.com>
2023-09-27 23:39:58 +0530

    Create Remove-Connector.ps1

---
+++ Remove-Connector.ps1
@@ -0,0 +1,83 @@
+$connectorName = "Microsoft.SystemCenter.Advisor.DataConnector"
+$msName="localhost"
+Import-Module OperationsManager
+
+    $mg = new-object Microsoft.EnterpriseManagement.ManagementGroup $msName
+    $admin = $mg.GetConnectorFrameworkAdministration()
+    ##########################################################################################
+    # Configures a connector with the specified name.
+    ##########################################################################################
+    function New-Connector([String] $name)
+    {
+         $connectorForTest = $null;
+         foreach($connector in $admin.GetMonitoringConnectors())
+    {
+    if($connectorName.Name -eq ${name})
+    {
+         $connectorForTest = Get-SCOMConnector -id $connector.id
+    }
+    }
+    if ($connectorForTest -eq $null)
+    {
+         $testConnector = New-Object Microsoft.EnterpriseManagement.ConnectorFramework.ConnectorInfo
+         $testConnector.Name = $name
+         $testConnector.Description = "${name} Description"
+         $testConnector.DiscoveryDataIsManaged = $false
+         $connectorForTest = $admin.Setup($testConnector)
+         $connectorForTest.Initialize();
+    }
+    return $connectorForTest
+    }
+    ##########################################################################################
+    # Removes a connector with the specified name.
+    ##########################################################################################
+    function Remove-Connector([String] $name)
+    {
+        $testConnector = $null
+        foreach($connector in $admin.GetMonitoringConnectors())
+       {
+        if($connector.Name -eq ${name})
+       {
+         $testConnector = Get-SCOMConnector -id $connector.id
+       }
+      }
+     if ($testConnector -ne $null)
+     {
+        if($testConnector.Initialized)
+     {
+     foreach($alert in $testConnector.GetMonitoringAlerts())
+     {
+       $alert.ConnectorId = $null;
+       $alert.Update("Delete Connector");
+     }
+     $testConnector.Uninitialize()
+     }
+     $connectorIdForTest = $admin.Cleanup($testConnector)
+     }
+    }
+    ##########################################################################################
+    # Delete a connector's Subscription
+    ##########################################################################################
+    function Delete-Subscription([String] $name)
+    {
+      foreach($testconnector in $admin.GetMonitoringConnectors())
+      {
+      if($testconnector.Name -eq $name)
+      {
+        $connector = Get-SCOMConnector -id $testconnector.id
+      }
+    }
+    $subs = $admin.GetConnectorSubscriptions()
+    foreach($sub in $subs)
+    {
+      if($sub.MonitoringConnectorId -eq $connector.id)
+      {
+        $admin.DeleteConnectorSubscription($admin.GetConnectorSubscription($sub.Id))
+      }
+     }
+    }
+    #New-Connector $connectorName
+    write-host "Delete-Subscription"
+    Delete-Subscription $connectorName
+    write-host "Remove-Connector"
+    Remove-Connector $connectorName
